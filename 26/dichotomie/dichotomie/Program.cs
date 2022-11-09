ceaae809837e99dec9032fb4abb405eb5851e0f2
Neko <nicolas.ferrand38@gmail.com>
2022-11-09 16:37:47 +0100

    exercise done 09/11/22

---
+++ 26/dichotomie/dichotomie/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+string[] names = { "agathe", "berthe", "chloé", "cunégonde", "olga", "raymonde", "sidonie"};
+string userInput;
+bool lookForName = true;
+int[] checkAtMiddle = { 0, names.Length };
+bool found = false;
+Console.WriteLine("Please enter a name");
+userInput = Console.ReadLine().ToLower();
+userInput = Regex.Replace(userInput, "[^a-zÀ-ž]", "");
+
+while (lookForName && !found)
+{
+
+    int delta = (checkAtMiddle[1] - checkAtMiddle[0]) / 2;
+    int index = checkAtMiddle[0] + delta;
+    if (userInput.CompareTo(names[index]) == 0)
+    {
+        Console.WriteLine(userInput + " was found at index " + (index+1));
+        lookForName = false;
+        found = true;
+    }
+    else if (userInput.CompareTo(names[index]) >= 0)
+    {
+        checkAtMiddle[0] = index;
+    }
+    else
+    {
+        checkAtMiddle[1] = index;
+    }
+    if(delta == 0)
+    {
+        lookForName = false;
+    }   
+}
+if (!found)
+    Console.WriteLine("0");
---
+++ 27/PGCD/PGCD/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+int number1;
+int number2;
+string userInput;
+
+Console.WriteLine("Enter number 1");
+userInput = Console.ReadLine();
+userInput = Regex.Replace(userInput, "[^0-9]","");
+number1 = int.Parse(userInput);
+
+Console.WriteLine("Enter number 2");
+userInput = Console.ReadLine();
+userInput = Regex.Replace(userInput, "[^0-9]", "");
+number2 = int.Parse(userInput);
+
+while(number1 != number2)
+{
+    if (number1 > number2)
+    {
+        number1 = number1 - number2;
+    }
+    else
+    {
+        number2 = number2 - number1;
+    }
+}
+Console.WriteLine("PGCD = " + number1);
