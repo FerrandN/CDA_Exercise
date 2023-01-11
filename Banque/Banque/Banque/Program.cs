13d42c5bb5e1fb90eda6eb5ca2b536df12b5d083
FerrandN <92740012+FerrandN@users.noreply.github.com>
2023-01-11 09:28:11 +0100

    11/01/23

---
+++ Banque/Banque/Banque/Program.cs
@@ -0,0 +1,14 @@
+﻿using BankAccount;
+using Banques;
+
+Bank myBank = new Bank("LBP","Lyon");
+myBank.AddAccount(1000, 0, 200, "Rodolphe");
+myBank.AddAccount(1500, 1, 200, "Gerard");
+myBank.AddAccount(100, 2, 200, "Mathieu");
+myBank.AddAccount(90000, 3, 200, "Nicolas");
+
+myBank.DisplayInformation();
+myBank.returnAccountWithHighestBalance().DisplayInformation();
+
+myBank.Accounts[1].Transfert(50, myBank.Accounts[2]);
+bool result = myBank.transfertValueToOtherBankAccount(myBank.Accounts[1],myBank.Accounts[2],500);
